e1205ba64c8b6756863d6d02698cd47e17d1ab05
Bence Ladoczki <info@ldczk.com>
2019-08-09 06:23:22 +0900

    categories

--- BigBuy1.py
+++ BigBuy1.py
@@ -87,7 +87,12 @@ for i in range(len(productsjson)):
     smalldescription = description[:100]
     title = thisinformation['name']
     sku  = thisinformation['sku']
+
+# Please get this from https://api.bigbuy.eu/rest/catalog/categories.json?isoCode=ru :
     category= thisproduct['category']
+# Please get this from https://api.bigbuy.eu/rest/catalog/categories.json?isoCode=ru :
+    parent_category = thisproduct['category']
+
     style= "NAN"
     color= "NAN"
     gender = "NAN"
@@ -109,6 +114,8 @@ for i in range(len(productsjson)):
                 'extended_description':     data[2],
                 'title':                    data[3],
                 'sku':                      data[4],
+# TODO this variable
+                'parent_category':          data[5],
                 'category':                 data[5],
                 'style':                    data[6],
                 'colour':                   data[7],
